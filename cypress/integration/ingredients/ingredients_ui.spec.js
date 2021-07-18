f9c1cd0f00d124df91bea8fcb10d1815720052a8
Christopher Dührkop <christopher.duehrkop@maurer-electronics.de>
2021-07-18 19:09:05 +0200

    new indentation size 2

--- cypress/integration/ingredients/ingredients_ui.spec.js
+++ cypress/integration/ingredients/ingredients_ui.spec.js
@@ -1,86 +1,87 @@
 /// <reference types="cypress" />
 
 context('Ingredients - UI Tests', () => {
-    beforeEach(() => {
-        cy.resetIngredientsTable();
-        cy.visit(Cypress.env('appUrl_Ingredients'));
-    })
+  beforeEach(() => {
+    cy.resetIngredientsTable();
+    cy.visit(Cypress.env('appUrl_Ingredients'));
+  })
 
-    /* General tests */
-    it('All ingredients show their current amount', () => {
-        cy.get('#form-Coffee').find('label').contains('Coffee: 10');
-        cy.get('#form-Water').find('label').contains('Water: 10');
-        cy.get('#form-Milk').find('label').contains('Milk: 10');
-        cy.get('#form-Cocoa').find('label').contains('Cocoa: 10');
-    })
+  /* General tests */
+  it('All ingredients show their current amount', () => {
+    cy.get('#form-Coffee').find('label').contains('Coffee: 10');
+    cy.get('#form-Water').find('label').contains('Water: 10');
+    cy.get('#form-Milk').find('label').contains('Milk: 10');
+    cy.get('#form-Cocoa').find('label').contains('Cocoa: 10');
+  })
 
-    it('All ingredients show their correct unit', () => {
-        cy.get('#form-Coffee').find('label').contains('g');
-        cy.get('#form-Water').find('label').contains('ml');
-        cy.get('#form-Milk').find('label').contains('ml');
-        cy.get('#form-Cocoa').find('label').contains('g');
-    })
+  it('All ingredients show their correct unit', () => {
+    cy.get('#form-Coffee').find('label').contains('g');
+    cy.get('#form-Water').find('label').contains('ml');
+    cy.get('#form-Milk').find('label').contains('ml');
+    cy.get('#form-Cocoa').find('label').contains('g');
+  })
 
-    /* User interactions take effect */
-    it('Ingredient amounts are updated after changes', () => {
-        cy.get('#amount-Water').type(1);
-        cy.get('#button-Water').should('be.enabled').click();
-        cy.get('#form-Water').find('label').contains('Water: 11ml');
-    })
+  /* User interactions take effect */
+  it('Ingredient amounts are updated after changes', () => {
+    cy.get('#amount-Water').type(1);
+    cy.get('#button-Water').should('be.enabled').click();
+    cy.get('#form-Water').find('label').contains('Water: 11ml');
+  })
 
-    it('Submit button disabled before interaction', () => {
-        cy.get('#amount-Coffee').click();
-        cy.get('#button-Coffee').should('be.disabled');
-    })
+  it('Submit button disabled before interaction', () => {
+    cy.get('#amount-Coffee').click();
+    cy.get('#button-Coffee').should('be.disabled');
+  })
 
-    it('Submit button disabled by empty input', () => {
-        cy.get('#amount-Coffee').click();
-        cy.get('#ingredient-Coffee').find('label').click();
-        cy.get('#button-Coffee').should('be.disabled');
-    })
+  it('Submit button disabled by empty input', () => {
+    cy.get('#amount-Coffee').click();
+    cy.get('#ingredient-Coffee').find('label').click();
+    cy.get('#button-Coffee').should('be.disabled');
+  })
 
-    it('Submit button enabled after typing value', () => {
-        cy.get('#amount-Water').type(1);
-        cy.get('#button-Water').should('be.enabled');
-    })
+  it('Submit button enabled after typing value', () => {
+    cy.get('#amount-Water').type(1);
+    cy.get('#button-Water').should('be.enabled');
+  })
 
-    /* Alerts behave correctly */
-    it('No alerts displayed before interaction', () => {
-        cy.get('.alert').should('not.exist');
-    })
+  /* Alerts behave correctly */
+  it('No alerts displayed before interaction', () => {
+    cy.get('.alert').should('not.exist');
+  })
 
-    it('Empty amount triggers alert', () => {
-        cy.get('#amount-Coffee').click();
-        cy.get('#ingredient-Coffee').find('label').click();
-        cy.get('.alert').should('be.visible');
-    })
+  it('Empty amount triggers alert', () => {
+    cy.get('#amount-Coffee').click();
+    cy.get('#ingredient-Coffee').find('label').click();
+    cy.get('.alert').should('be.visible');
+  })
 
-    it('Empty amount alert text correct', () => {
-        cy.get('#amount-Coffee').click();
-        cy.get('#ingredient-Coffee').find('label').click();
-        cy.get('.alert').should('be.visible');
-    })
+  it('Empty amount alert text correct', () => {
+    cy.get('#amount-Coffee').click();
+    cy.get('#ingredient-Coffee').find('label').click();
+    cy.get('.alert').should('be.visible');
+  })
 
-    it('Empty amount alert disappears after inserting one', () => {
-        cy.get('#amount-Milk').type(1);
-        cy.get('#button-Milk').should('be.enabled').click();
-        cy.get('.alert').should('not.exist');
-    })
+  it('Empty amount alert disappears after inserting one', () => {
+    cy.get('#amount-Milk').type(1);
+    cy.get('#button-Milk').should('be.enabled').click();
+    cy.get('.alert').should('not.exist');
+  })
 
-    /* Page length doesn't cause issues */
-    it('Bottommost ingredient can be reached', () => {
-        cy.scrollTo('bottom');
-        cy.get('#ingredient-Cocoa').should('be.visible');
-    })
+  /* Page length doesn't cause issues */
+  it('Bottommost ingredient can be reached', () => {
+    cy.scrollTo('bottom');
+    cy.get('#ingredient-Cocoa').should('be.visible');
+  })
 
-    it('Bottommost ingredient not scrolled out of view after interaction', () => {
-        cy.get('#amount-Cocoa').scrollIntoView();
-        cy.wait(500);
-        cy.get('#amount-Cocoa').should('be.visible')
-            .type(1);
-        cy.get('#button-Cocoa').should('be.enabled')
-            .click();
-        cy.get('#ingredient-Cocoa').should('be.visible');
-        cy.get('#form-Cocoa').find('label').contains('Cocoa: 11g');
-    })
+  it.only('Bottommost ingredient not scrolled out of view after interaction', () => {
+    cy.get('#amount-Cocoa').scrollIntoView();
+    //cy.wait(500);
+    cy.get('#amount-Cocoa')
+      .should('be.visible')
+      .type(1);
+    cy.get('#button-Cocoa').should('be.enabled')
+      .click();
+    cy.get('#ingredient-Cocoa').should('be.visible');
+    cy.get('#form-Cocoa').find('label').contains('Cocoa: 11g');
+  })
 })
--- src/app/components/ingredient-item/ingredient-item.component.ts
+++ src/app/components/ingredient-item/ingredient-item.component.ts
@@ -3,24 +3,24 @@ import { IngredientsService } from 'src/app/services/ingredients.service';
 import { Ingredient } from 'src/app/models/Ingredient';
 
 @Component({
-    selector: 'app-ingredient-item',
-    templateUrl: './ingredient-item.component.html',
-    styleUrls: ['../../app.component.css', './ingredient-item.component.css']
+  selector: 'app-ingredient-item',
+  templateUrl: './ingredient-item.component.html',
+  styleUrls: ['../../app.component.css', './ingredient-item.component.css']
 })
 
 export class IngredientItemComponent implements OnInit {
-    @Input() ingredient: Ingredient = new Ingredient;
+  @Input() ingredient: Ingredient = new Ingredient;
 
-    refillAmountValue!: number;
+  refillAmountValue!: number;
 
-    constructor(private service: IngredientsService) { }
+  constructor(private service: IngredientsService) { }
 
-    ngOnInit() { }
+  ngOnInit() { }
 
-    log(x: any) { console.log(x); }
+  log(x: any) { console.log(x); }
 
-    onRefillSubmit() {
-        // console.log("Updated ingredient: " + this.ingredient.name + " with " + this.ingredient.amount);
-        this.service.refillIngredient(this.ingredient, this.refillAmountValue);
-    }
+  onRefillSubmit() {
+    // console.log("Updated ingredient: " + this.ingredient.name + " with " + this.ingredient.amount);
+    this.service.refillIngredient(this.ingredient, this.refillAmountValue);
+  }
 }
