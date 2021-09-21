0f784de9af0ecdb946c82543ffddf102f067a19f
Christopher Dührkop <christopher.duehrkop@maurer-electronics.de>
2021-09-22 00:05:29 +0200

    Add DB ingredients tests

--- cypress/integration/ingredients/ingredients_db.spec.js
+++ cypress/integration/ingredients/ingredients_db.spec.js
@@ -10,12 +10,38 @@ context('Ingredients - DB Tests', () => {
     cy.resetIngredientsTable();
   })
 
-  /* General tests */
+  /* Default values - Test via request */
+  it('Default amount is pre-set correctly in DB', () => {
+    cy.request('GET', Cypress.env('dbUrl_Ingredients') + '/1').then(response => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property('name', 'Coffee');
+      expect(response.body).to.have.property('amount', 10);
+    });
+  })
 
-  /* User interactions take effect */
-  it('Ingredient amounts are updated after changes', () => {
+  /* Change via UI - Test via request */
+  it('Ingredient amount is updated after UI interaction', () => {
     cy.get('#amount-Water').type(1);
     cy.get('#button-Water').should('be.enabled').click();
-    cy.get('#form-Water').find('label').contains('Water: 11ml');
+    cy.request('GET', Cypress.env('dbUrl_Ingredients') + '/2').then(response => {
+      expect(response.body).to.have.property('name', 'Water');
+      expect(response.body).to.have.property('amount', 11);
+    });
   })
+
+  /* Change via request - Test via request */
+  it('Ingredient amount is updated after PUT request', () => {
+    cy.request('PUT', Cypress.env('dbUrl_Ingredients') + '/3', empty_milk);
+    cy.request('GET', Cypress.env('dbUrl_Ingredients') + '/3').then(response => {
+      expect(response.body).to.have.property('name', 'Milk');
+      expect(response.body).to.have.property('amount', 0);
+    });
+  })
+
+  const empty_milk = {
+    "id": 3,
+    "name": "Milk",
+    "unit": "ml",
+    "amount": 0
+  }
 })
---
+++ cypress/integration/ingredients/ingredients_logic.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+context('Ingredients - Logic Tests', () => {
+  beforeEach(() => {
+    cy.resetIngredientsTable();
+    cy.visit('http://localhost:4200/ingredients');
+  })
+
+  after(() => {
+    cy.resetIngredientsTable();
+  })
+
+  /* General tests */
+
+  /* User interactions take effect */
+  it('Ingredient amounts are updated after changes', () => {
+    cy.get('#amount-Water').type(1);
+    cy.get('#button-Water').should('be.enabled').click();
+    cy.get('#form-Water').find('label').contains('Water: 11ml');
+  })
+})
